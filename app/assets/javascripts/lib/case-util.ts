479de766ccc59f0631796bdde7a533cf2e8a9eca
tai2 <hello@tai2.net>
2017-09-24 21:55:02 +0900

    Fix lint error in case-util.ts

--- app/assets/javascripts/lib/case-util.ts
+++ app/assets/javascripts/lib/case-util.ts
@@ -3,14 +3,14 @@ import * as _ from 'lodash'
 type DictToDict = (obj: _.Dictionary<any>) => _.Dictionary<any>
 
 export function snakeCaseKeys(obj: object): object {
-  const tr: DictToDict = (obj) => _.transform(obj, (result, value, key) => {
+  const tr: DictToDict = (o) => _.transform(o, (result, value, key) => {
     result[_.snakeCase(key)] = (_.isPlainObject(value) ? tr(value) : value)
   })
   return tr(obj)
 }
 
 export function camelCaseKeys(obj: object): object {
-  const tr: DictToDict = (obj) => _.transform(obj, (result, value, key) => {
+  const tr: DictToDict = (o) => _.transform(o, (result, value, key) => {
     result[_.camelCase(key)] = _.isPlainObject(value) ? tr(value) : value
   })
   return tr(obj)
