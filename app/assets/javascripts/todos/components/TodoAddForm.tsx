a8f5cc267b33a7a53e3f3bc98ae5e03ac52d455b
tai2 <hello@tai2.net>
2017-09-24 22:38:53 +0900

    Fix lint error in TodoAddForm.tsx

--- app/assets/javascripts/todos/components/TodoAddForm.tsx
+++ app/assets/javascripts/todos/components/TodoAddForm.tsx
@@ -3,8 +3,7 @@ import * as moment from 'moment'
 import * as React from 'react'
 import * as DateTime from 'react-datetime'
 import { connect, Dispatch } from 'react-redux'
-import { addTodoRequested } from '../actions'
-import { Action } from '../actions'
+import { Action, addTodoRequested } from '../actions'
 import styles from './TodoAddForm.scss'
 
 interface Props {
@@ -24,11 +23,14 @@ class TodoAddForm extends React.Component<Props, State> {
       dueDate: new Date(),
     }
   }
-  public handleAddTodo() {
+  public handleAddTodo = () => {
     this.setState({ content: '', dueDate: new Date() })
     this.props.onAddTodo(this.state.content, this.state.dueDate)
   }
-  public handleChangeDueDate(dt: React.ChangeEvent<any> | moment.Moment | string) {
+  public handleChangeContent = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ content: ev.currentTarget.value })
+  }
+  public handleChangeDueDate = (dt: React.ChangeEvent<any> | moment.Moment | string) => {
     if (moment.isMoment(dt)) {
       this.setState({ dueDate: dt.toDate() })
     }
@@ -38,10 +40,11 @@ class TodoAddForm extends React.Component<Props, State> {
       <div className="form-inline">
         <div className={classNames('form-group', styles.item)}>
           <label>Content:&nbsp;
-            <input className={styles.content}
+            <input
+              className={styles.content}
               type="text"
               value={this.state.content}
-              onChange={(e) => this.setState({ content: e.target.value })}
+              onChange={this.handleChangeContent}
             />
           </label>
         </div>
@@ -50,14 +53,16 @@ class TodoAddForm extends React.Component<Props, State> {
             <DateTime
               className={styles.dueDate}
               value={this.state.dueDate}
-              onChange={(dt) => this.handleChangeDueDate(dt)}
+              onChange={this.handleChangeDueDate}
             />
           </label>
         </div>
         <button
           className={classNames('btn btn-default', styles.item)}
-          onClick={() => this.handleAddTodo()}
-        >Create Todo</button>
+          onClick={this.handleAddTodo}
+        >
+          Create Todo
+        </button>
       </div>
     )
   }
