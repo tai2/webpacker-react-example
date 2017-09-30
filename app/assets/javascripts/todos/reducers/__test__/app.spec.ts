c1c8074f08e56d79fa922d9bed2201fd799e86b1
tai2 <hello@tai2.net>
2017-09-30 16:54:24 +0900

    Fix lint errors.

--- app/assets/javascripts/todos/reducers/__test__/app.spec.ts
+++ app/assets/javascripts/todos/reducers/__test__/app.spec.ts
@@ -1,13 +1,13 @@
-import 'mocha';
-import * as assert from 'assert';
+import * as assert from 'assert'
+import 'mocha'
 import * as actions from '../../actions'
-import appReducer, { AppState, initialState, SINGLETON_ID } from '../app';
+import appReducer, { AppState, initialState, SINGLETON_ID } from '../app'
 
 describe('App reducer', () => {
   describe('ToggleDoneFilter Action', () => {
     it('should toggles doneFilter state', () => {
       const newState = appReducer(initialState, actions.toggleDoneFilter())
-      assert(newState.doneFilter !== initialState.doneFilter);
+      assert(newState.doneFilter !== initialState.doneFilter)
     })
   })
 
@@ -16,19 +16,19 @@ describe('App reducer', () => {
       const state: AppState = {
         ...initialState,
         sortBy: 'createdAt',
-        sortOrder: 'asc'
+        sortOrder: 'asc',
       }
       const newState = appReducer(state, actions.selectOrder('dueDate', 'desc'))
-      assert(newState.sortBy === 'dueDate');
-      assert(newState.sortOrder === 'desc');
+      assert(newState.sortBy === 'dueDate')
+      assert(newState.sortOrder === 'desc')
     })
   })
 
   describe('AddTodoRequested Action', () => {
     it('should set requesting to true and error to null', () => {
       const newState = appReducer(initialState, actions.addTodoRequested('todo', '2017-09-30T07:32:08.591Z'))
-      assert(newState.requests.addTodo[SINGLETON_ID].requesting);
-      assert(newState.requests.addTodo[SINGLETON_ID].error === null);
+      assert(newState.requests.addTodo[SINGLETON_ID].requesting)
+      assert(newState.requests.addTodo[SINGLETON_ID].error === null)
     })
   })
 })
