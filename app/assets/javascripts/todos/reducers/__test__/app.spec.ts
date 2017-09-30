a385499b3192c14a7bca5e37f8a2a13aa689b58f
tai2 <hello@tai2.net>
2017-09-30 17:19:19 +0900

    Use more proper assertion

--- app/assets/javascripts/todos/reducers/__test__/app.spec.ts
+++ app/assets/javascripts/todos/reducers/__test__/app.spec.ts
@@ -39,8 +39,10 @@ describe('App reducer', () => {
   describe('AddTodoRequested Action', () => {
     it('should set requesting to true and error to null', () => {
       const newState = appReducer(initialState, actions.addTodoRequested('todo', '2017-09-30T07:32:08.591Z'))
-      assert(newState.requests.addTodo[SINGLETON_ID]!.requesting)
-      assert.equal(newState.requests.addTodo[SINGLETON_ID]!.error, null)
+      assert.deepEqual(newState.requests.addTodo[SINGLETON_ID]!, {
+        requesting: true,
+        error: null,
+      })
     })
   })
 
