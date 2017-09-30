5d7ff4d86defebc8061f17a35bb01bf8214a0153
tai2 <hello@tai2.net>
2017-09-30 16:26:26 +0900

    reducer test for SelectOrder Action

--- app/assets/javascripts/todos/reducers/__test__/app.spec.ts
+++ app/assets/javascripts/todos/reducers/__test__/app.spec.ts
@@ -1,12 +1,26 @@
 import 'mocha';
 import * as assert from 'assert';
+import * as actions from '../../actions'
 import appReducer, { initialState } from '../app';
 
 describe('App reducer', () => {
   describe('ToggleDoneFilter Action', () => {
     it('should toggles doneFilter state', () => {
-      const newState = appReducer(initialState, { type: 'TOGGLE_DONE_FILTER' })
+      const newState = appReducer(initialState, actions.toggleDoneFilter())
       assert(newState.doneFilter != initialState.doneFilter);
+    })
+  })
+
+  describe('SelectOrder Action', () => {
+    it('should set sortBy and sortOrder state', () => {
+      const state = {
+        ...initialState,
+        sortBy: 'createdAt',
+        sortOrder: 'asc'
+      }
+      const newState = appReducer(state, actions.selectOrder('dueDate', 'desc'))
+      assert(newState.sortBy == 'dueDate');
+      assert(newState.sortOrder == 'desc');
     })
   })
 })
