95347b25e75b806bbcc9944bef21a66b8dd052f7
tai2 <hello@tai2.net>
2018-08-02 22:42:47 +0900

    Change hot HOC application order

--- app/assets/javascripts/todos/components/App/index.ts
+++ app/assets/javascripts/todos/components/App/index.ts
@@ -1,10 +1,10 @@
-import { flow } from 'lodash'
+import { flowRight } from 'lodash'
 import { hot } from 'react-hot-loader'
 import { connect } from 'react-redux'
 import { StoreState, visibleTodos } from '../../reducers'
 import App, { Props } from './render'
 
-export default flow([
+export default flowRight([
   hot(module),
   connect<Props>(({ todos, app }: StoreState) => ({
     todos: visibleTodos(todos, app.sortBy, app.sortOrder, app.doneFilter),
